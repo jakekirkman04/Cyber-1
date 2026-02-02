fc75fe72efbfc467001ce7974ed404f6830dca44
Jake Kirkman <jakekirkman04@gmail.com>
2026-02-02 12:04:45 +1100

    Move /merged to root route and add /landing for legacy support

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { Landing } from "./screens/Landing";
 import { DesktopOld } from "./screens/Old";
 import { Merged } from "./screens/Merged";
@@ -9,9 +9,10 @@ createRoot(document.getElementById("app") as HTMLElement).render(
   <StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<Landing />} />
+        <Route path="/" element={<Merged />} />
         <Route path="/old" element={<DesktopOld />} />
-        <Route path="/merged" element={<Merged />} />
+        <Route path="/merged" element={<Navigate to="/" replace />} />
+        <Route path="/landing" element={<Landing />} />
       </Routes>
     </Router>
   </StrictMode>,
